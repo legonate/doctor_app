0296259bded05700e61d84ac0e975d4c9d3b3a70
Liam Callahan <liam@acodecrafter@gmail.com>
2025-11-07 13:20:12 -0500

    Add basic control flow & create / list commands for doctors and patients

--- src/main/java/edu/secourse/patientportal/controllers/UserController.java
+++ src/main/java/edu/secourse/patientportal/controllers/UserController.java
@@ -1,20 +1,60 @@
 package edu.secourse.patientportal.controllers;
 
 import edu.secourse.patientportal.services.UserService;
+import java.util.Scanner;
 import edu.secourse.patientportal.models.*;
 
 public class UserController {
     UserService us;
+    Scanner sc;
 
-    public UserController() {
+    public UserController(Scanner sc) {
         us = new UserService();
+        this.sc = sc;
     }
 
-    public void createDoctor(String username, String email_address, String role, String name, String password) {
+    public void createDoctor() {
+        System.out.print("enter Username: ");
+        String username = sc.nextLine();
+        System.out.print("enter email: ");
+        String email_address = sc.nextLine();
+        System.out.print("enter role: ");
+        String role = sc.nextLine();
+        System.out.print("enter name: ");
+        String name = sc.nextLine();
+        System.out.print("enter password: ");
+        String password = sc.nextLine();
         this.us.addDoctor(username, email_address, role, name, password);
+        System.out.println("Doctor Created");
     }
 
-    public void createPatient(String username, String email_address, String role, String name, String password) {
-        this.us.addDoctor(username, email_address, role, name, password);
+    public void createPatient() {
+        System.out.print("enter Username: ");
+        String username = sc.nextLine();
+        System.out.print("enter email: ");
+        String email_address = sc.nextLine();
+        System.out.print("enter role: ");
+        String role = sc.nextLine();
+        System.out.print("enter name: ");
+        String name = sc.nextLine();
+        System.out.print("enter password: ");
+        String password = sc.nextLine();
+        this.us.addPatient(username, email_address, role, name, password);
+        System.out.println("Patient Created");
+    }
+
+    public void listPatients() {
+        for (User user : this.us.getUsers()) {
+            if (user instanceof Patient) {
+                System.out.println(user.getName());
+            }
+        }
+    }
+    public void listDoctors() {
+        for (User user : this.us.getUsers()) {
+            if (user instanceof Doctor) {
+                System.out.println(user.getName());
+            }
+        }
     }
 }
