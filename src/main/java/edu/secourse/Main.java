6fc1823c16d8061e336d943b5ca21a45210bd6ff
Ali Beheshti <alibeheshti519@gmail.com>
2025-11-17 14:51:33 -0500

    Update Main.java

--- src/main/java/edu/secourse/Main.java
+++ src/main/java/edu/secourse/Main.java
@@ -3,15 +3,26 @@ package edu.secourse;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+<<<<<<< Updated upstream
     static void main() {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         IO.println(String.format("Hello and welcome!"));
+=======
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println(String.format("Hello and welcome!"));
+>>>>>>> Stashed changes
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+<<<<<<< Updated upstream
             IO.println("i = " + i);
+=======
+            System.out.println("i = " + i);
+>>>>>>> Stashed changes
         }
     }
 }
